1206618b3285a79906a69e75d3c6f46d42739d95
atlasoptimization <jemilbutt.opt@gmail.com>
2024-03-23 13:03:43 +0100

    Add illustrations to phase_mean_and_variance_models.py

--- BAFU_processing/phase_mean_and_variance_models.py
+++ BAFU_processing/phase_mean_and_variance_models.py
@@ -46,8 +46,13 @@ info_mats_data = data_struct[0][0][1]
 nice_patch_unw = np.linspace(1000, 1010,11).astype(int)
 nice_patch_mli = np.linspace(1810, 1820,11).astype(int)
 many_patch_mli = np.linspace(1810, 2000,191).astype(int)
+all_patch_mli = np.linspace(0,19999,20000).astype(int)
 
-patch_choice = nice_patch_mli
+# iii) Data preselection
+
+n_illu = 4
+patch_choice = all_patch_mli
+illustration_choice = np.round(np.linspace(0, len(patch_choice)-1, n_illu)).astype(int)
 phase_vals_data = phase_vals_data[patch_choice,:]
 info_mats_data = info_mats_data[patch_choice,:,:]
 
@@ -57,7 +62,7 @@ data_std = info_mats_data.std(axis=(0, 1), keepdims=True)
 info_mats_data = (info_mats_data - data_mean) / (data_std + 1e-6)
 
 
-# iii) Definitions
+# iv) Definitions
 
 n_x = 20
 n_y = 20
@@ -73,6 +78,9 @@ x_vec = np.vstack((xx.flatten(), yy.flatten())).T
 
 pyro.clear_param_store()
 today = date.today()
+
+
+
 
 
 
@@ -149,7 +157,8 @@ class FullData():
         for name, attribute in base_data.__dict__.items():
             setattr(self, name, attribute)
         # Add simulations
-        self.phase_mats = phase_mats            
+        self.phase_mats = phase_mats     
+        self.phase_mats_illu = phase_mats[illustration_choice,:]
 
 full_data = FullData(base_data, phase_mats)
 
@@ -196,7 +205,7 @@ def var_function(regression_data, beta_0, beta_1, beta_2, simplicity = False):
 
 # iii) Stochastic model
 
-subsample_size = 11
+subsample_size = 512
 simplicity = False
 
 def model(regression_data, observations = None):
@@ -228,14 +237,39 @@ def model(regression_data, observations = None):
     
     return phase_sample, mu, sigma
 
-simulation_pretrain, mu_pretrain, sigma_pretrain = copy.copy(model(regression_data))
-simulation_pretrain = simulation_pretrain.reshape([subsample_size, n_x,n_y])   
+pyro.render_model(model, model_args=(regression_data,), render_distributions=True, render_params=True)
 
 
-# iv) Guide
+# iv) Guide and eval model
 
 def guide(regression_data, observations = None):
     pass
+
+def eval_model(regression_data, observations = None):
+    # Instantiate parameters
+    alpha_0 = pyro.get_param_store()['alpha_0'].detach()
+    alpha_1 = pyro.get_param_store()['alpha_1'].detach()
+    alpha_2 = pyro.get_param_store()['alpha_2'].detach()
+    
+    beta_0 = pyro.get_param_store()['beta_0'].detach()
+    beta_1 = pyro.get_param_store()['beta_1'].detach()
+    beta_2 = pyro.get_param_store()['beta_2'].detach()
+        
+    # Determine distribution and sample from it
+    with pyro.plate('batch_plate_eval', size = n_illu, dim = -3):
+        mu = mean_function(regression_data, alpha_0, alpha_1, alpha_2, simplicity = simplicity)
+        sigma = var_function(regression_data, beta_0, beta_1, beta_2, simplicity = simplicity)
+        data_dist = pyro.distributions.Normal(mu, sigma)
+        with pyro.plate('y_plate_eval', size = n_y, dim = -2):
+            with pyro.plate('x_plate_eval', size = n_x, dim = -1):
+                phase_sample = pyro.sample('phase_sample_eval', data_dist)
+    
+    return phase_sample, mu, sigma
+
+simulation_pretrain, mu_pretrain, sigma_pretrain = copy.copy(eval_model(regression_data[illustration_choice,...]))
+simulation_pretrain = simulation_pretrain.reshape([-1, n_x,n_y]) 
+
+
 
 
 """
@@ -245,7 +279,7 @@ def guide(regression_data, observations = None):
 # i) Set up training
 
 # specifying scalar options
-learning_rate = 1e-4
+learning_rate = 1e-5
 num_epochs = 10000
 adam_args = {"lr" : learning_rate}
 
@@ -264,24 +298,24 @@ for epoch in range(num_epochs):
     epoch_loss = svi.step(regression_data, phase_mats)
     train_elbo.append(epoch_loss)
     
-    # # Check if the current model is better than what we've seen before
-    # if epoch_loss < 0.99*best_loss:
-    #     best_loss = epoch_loss
-    #     # Save the model
-    #     pyro.get_param_store().save(model_save_path)
-    #     print(f"Saved the model at epoch {epoch} with loss {best_loss}")
+    # Check if the current model is better than what we've seen before
+    if epoch_loss < 0.99*best_loss:
+        best_loss = epoch_loss
+        # Save the model
+        pyro.get_param_store().save(model_save_path)
+        print(f"Saved the model at epoch {epoch} with loss {best_loss}")
     
     if epoch % 100 == 0:
         print("Epoch : {} train loss : {}".format(epoch, epoch_loss))
 
 # Load best parameters
-# pyro.get_param_store().load(model_save_path)
+pyro.get_param_store().load(model_save_path)
 
 
 # iii) Simulation posttraining
 
-simulation_posttrain, mu_posttrain, sigma_posttrain = copy.copy(model(regression_data))
-simulation_posttrain = simulation_posttrain.reshape([subsample_size, n_x,n_y])
+simulation_posttrain, mu_posttrain, sigma_posttrain = copy.copy(eval_model(regression_data[illustration_choice,...]))
+simulation_posttrain = simulation_posttrain.reshape([-1, n_x,n_y])
 
 
 
@@ -301,16 +335,16 @@ plt.ylabel('Loss')
 
 # i) Illustrate data
 
-fig, axs = plt.subplots(3, 4, figsize=(15, 9))
+fig, axs = plt.subplots(n_illu, 4, figsize=(15, 15))
 
 attributes = [full_data.phase_mats, full_data.coherence_mats, full_data.z_mats, full_data.aoi_mats]
 titles = ["Phase Mats", "Coherence Mats", "Elevation Mats", "AOI Mats"]
 
-for i in range(3):  # loop over samples
+for i in range(n_illu):  # loop over samples
     for j in range(4):  # loop over attributes
         ax = axs[i, j]
         attribute = attributes[j]
-        ax.imshow(attribute[i, :, :])
+        ax.imshow(attribute[illustration_choice[i], :, :])
         if i == 0:
             ax.set_title(titles[j])
 
@@ -322,12 +356,12 @@ plt.show()
 
 # ii) Illustrate data, initial simulations, trained simulations
 
-fig, axs = plt.subplots(5, 3, figsize=(9, 15))
+fig, axs = plt.subplots(n_illu, 3, figsize=(9, 15))
 
-attributes = [full_data.phase_mats, simulation_pretrain.detach(), simulation_posttrain.detach()]
+attributes = [full_data.phase_mats_illu, simulation_pretrain.detach(), simulation_posttrain.detach()]
 titles = ["Phase: data", "pretrain", " posttrain"]
 
-for i in range(5):  # loop over samples
+for i in range(n_illu):  # loop over samples
     for j in range(3):  # loop over attributes
         ax = axs[i, j]
         attribute = attributes[j]
@@ -343,61 +377,98 @@ plt.show()
 
 # iii) Illustrate mean and variance
 
-fig, axs = plt.subplots(2, 5, figsize=(10, 5))
+def illustrate_mean_and_var(image_nr):
+    
+    fig, axs = plt.subplots(2, 5, figsize=(10, 5))
+    
+    axs[0,0].imshow(full_data.coherence_mats[illustration_choice[image_nr],:,:])
+    axs[0,1].imshow(full_data.x_mats[illustration_choice[image_nr],:,:])
+    axs[0,2].imshow(full_data.y_mats[illustration_choice[image_nr],:,:])
+    axs[0,3].imshow(full_data.z_mats[illustration_choice[image_nr],:,:])
+    axs[0,4].imshow(full_data.aoi_mats[illustration_choice[image_nr],:,:])
+    
+    axs[1,1].imshow(mu_pretrain[image_nr,:,:].detach())
+    axs[1,2].imshow(sigma_pretrain[image_nr,:,:].detach())
+    axs[1,3].imshow(mu_posttrain[image_nr,:,:].detach())
+    axs[1,4].imshow(sigma_posttrain[image_nr,:,:].detach())
+    
+    titles = ["Coherence", "x values", "y values", "z values", "AOI", "", \
+              "mu pretrain", "sigma pretrain" , "mu posttrain" , "sigma posttrain"]
+    
+    for i in range(2):  # loop over samples
+        for j in range(5):  # loop over attributes
+            ax = axs[i,j]
+            ax.set_title(titles[5*i+j])
+            ax.axis('off')
+    
+    plt.tight_layout()
+    plt.show()
 
-axs[0,0].imshow(full_data.coherence_mats[0,:,:])
-axs[0,1].imshow(full_data.x_mats[0,:,:])
-axs[0,2].imshow(full_data.y_mats[0,:,:])
-axs[0,3].imshow(full_data.z_mats[0,:,:])
-axs[0,4].imshow(full_data.aoi_mats[0,:,:])
-
-axs[1,1].imshow(mu_pretrain[0,:,:].detach())
-axs[1,2].imshow(sigma_pretrain[0,:,:].detach())
-axs[1,3].imshow(mu_posttrain[0,:,:].detach())
-axs[1,4].imshow(sigma_posttrain[0,:,:].detach())
-
-titles = ["Coherence", "x values", "y values", "z values", "AOI", "", \
-          "mu pretrain", "sigma pretrain" , "mu posttrain" , "sigma posttrain"]
-
-for i in range(2):  # loop over samples
-    for j in range(5):  # loop over attributes
-        ax = axs[i,j]
-        ax.set_title(titles[5*i+j])
-        ax.axis('off')
-
-plt.tight_layout()
-plt.show()
+illustrate_mean_and_var(0)
+illustrate_mean_and_var(1)
+illustrate_mean_and_var(2)
+illustrate_mean_and_var(3)
 
 
 # iv) Illustrate the parameters
+
+param_names_1 = ['offset', 'x', 'y', 'z', 'coh', 'aoi']
+param_names_2 = ['x', 'y', 'z', 'coh', 'aoi']
 
 param_dict = {}
 for param, value in pyro.get_param_store().items():
     param_dict[param] = value
 
-fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+fig, axs = plt.subplots(2, 2, figsize=(13, 10))
 # alpha values
-axs[0,0].bar(torch.linspace(0,n_regression_vars,n_regression_vars+1),
+x_values_1 = torch.linspace(0,n_regression_vars,n_regression_vars+1)
+x_values_2 = x_values_1[0:-1]
+axs[0,0].bar(x_values_1,
              torch.cat((param_dict['alpha_0'].detach().reshape([1]),
                         param_dict['alpha_1'].detach())))
 axs[0,0].set_xlabel('alpha')
+axs[0,0].set_xticks(x_values_1)
+axs[0,0].set_xticklabels(param_names_1)
 axs[0,0].set_ylabel('param value')
 axs[0,0].set_title('Parameter values alpha_0, alpha_1')
 
-axs[0,1].imshow(param_dict['alpha_2'].detach())
+im_alpha_2 = axs[0,1].imshow(param_dict['alpha_2'].detach())
 axs[0,1].set_xlabel('alpha_2_ij')
+axs[0,1].set_xticks(x_values_2)
+axs[0,1].set_xticklabels(param_names_2)
 axs[0,1].set_ylabel('alpha_2_ij')
+axs[0,1].set_yticks(x_values_2)
+axs[0,1].set_yticklabels(param_names_2)
 axs[0,1].set_title('Parameter values alpha_2')
+plt.colorbar(im_alpha_2, ax=axs[0,1])
 
 # beta_values
 axs[1,0].bar(torch.linspace(0,n_regression_vars,n_regression_vars+1),
              torch.cat((param_dict['beta_0'].detach().reshape([1]),
                         param_dict['beta_1'].detach())))
 axs[1,0].set_xlabel('beta')
+axs[1,0].set_xticks(x_values_1)
+axs[1,0].set_xticklabels(param_names_1)
 axs[1,0].set_ylabel('param value')
 axs[1,0].set_title('Parameter values beta_0, beta_1')
 
-axs[1,1].imshow(param_dict['beta_2'].detach())
+im_beta_2 = axs[1,1].imshow(param_dict['beta_2'].detach())
 axs[1,1].set_xlabel('beta_2_ij')
+axs[1,1].set_xticks(x_values_2)
+axs[1,1].set_xticklabels(param_names_2)
 axs[1,1].set_ylabel('beta_2_ij')
+axs[1,1].set_yticks(x_values_2)
+axs[1,1].set_yticklabels(param_names_2)
 axs[1,1].set_title('Parameter values beta_2')
+plt.colorbar(im_beta_2, ax=axs[1,1])
+
+plt.tight_layout()
+plt.show()
+
+
+# v) Illustrate mean and variance
+
+
+
+
+
